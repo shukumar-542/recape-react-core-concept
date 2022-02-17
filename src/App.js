7f94afd3a14f859cfb557322e7de082fece57b08
unknown <shukumar-542>
2022-02-17 12:42:10 +0600

    recape react core concepsts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  // const friendsNames = [
+  //   { name:'Sharlok Homes'},
+  //   {name: 'Dr Watson'},
+  //   { name :'Bomkeyes Bokshi'},
+  //   { name: 'Felu Da'}
+  // ]
+   const [friendsNames, setFriends] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setFriends(data))
+  },[])
+
   return (
+    
     <div className="App">
+      
+      <FriendCounter></FriendCounter>
+
+
+      {
+        friendsNames.map(friend => <Frieds friend ={friend} key={friend.id}></Frieds>)
+      }
+    
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
 }
+function FriendCounter(){
+  const [count, setCount] = useState(4)
+  
+  return(
+    <div>
+      <button onClick={() =>{setCount(count +1)}}>Add friend</button>
+      <h3>Total Friends :  {count} </h3>
+      <NewFriends new ={count}></NewFriends>
+    </div>
+  )
+}
+
+function NewFriends(props){
+  return(
+    <div> New Friends : {props.new}</div>
+  )
+}
+
+ function Frieds(props){
+  // console.log(props);
+  const friendStyle ={
+       border : "1px solid purple",
+       margin :'20px',
+       backgroundColor : 'purple',
+       color : 'white',
+  }
+  const {name} = props.friend
+
+   return (
+ 
+     <div style={friendStyle}>
+       <h1>Name:{name}</h1>
+
+     </div>
+   )
+ }
 
 export default App;
